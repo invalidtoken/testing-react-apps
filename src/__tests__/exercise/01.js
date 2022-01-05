2c2cb0cf7a70f4fda6ae53d0405edd96b5a0b78b
invalidtoken <codeit.sahil98@gmail.com>
2022-01-05 21:41:21 +0530

    added basic test

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -5,25 +5,35 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use ReactDOM.render to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 increment.click())
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 decrement.click())
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+// cleanup
+beforeEach(() => {
+  document.body.innerHTML = ''
 })
 
-/* eslint no-unused-vars:0 */
+test('counter increments and decrements when the buttons are clicked', () => {
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+
+  ReactDOM.render(<Counter />, div)
+
+  const message = div.firstChild.querySelector('div')
+  expect(message.textContent).toBe('Current count: 0')
+
+  const [decrementButton, incrementButton] =
+    document.body.querySelectorAll('button')
+
+  const mouseEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  })
+
+  decrementButton.dispatchEvent(mouseEvent)
+  expect(message.textContent).toBe('Current count: -1')
+
+  incrementButton.dispatchEvent(mouseEvent)
+  expect(message.textContent).toBe('Current count: 0')
+
+  incrementButton.dispatchEvent(mouseEvent)
+  expect(message.textContent).toBe('Current count: 1')
+})
